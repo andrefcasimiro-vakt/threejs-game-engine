9810b268dbde14c3372282aa488d61fca535565a
andre fernandes <andrefcasimiro@gmail.com>
2020-10-31 13:23:41 +0000

    Updating inputs

--- src/editor/editor-toolbar.ts
+++ src/editor/editor-toolbar.ts
@@ -4,6 +4,9 @@ import { ChangeEvent } from "../typescript/typings";
 import Scene from "../core/scene";
 import EditorGrid from "./editor-grid";
 
+const GRID_MAX_SIZE = 30
+const GRID_MIN_SIZE = 1
+
 export default class EditorToolbar implements Component {
   private _inputs: Input[]
   private _requestAnimationFrameId: number
@@ -30,54 +33,88 @@ export default class EditorToolbar implements Component {
     window.cancelAnimationFrame(this._requestAnimationFrameId)
   }
 
+  clampGridInput = (value: number): number => {
+    if (value > GRID_MAX_SIZE) return GRID_MAX_SIZE
+    if (value < GRID_MIN_SIZE) return GRID_MIN_SIZE
+
+    return value
+  }
 
   renderInputs = () => {
     this._inputs = [
       new Input({
       name: 'sceneName',
-      placeholder: 'Insert a scene name...',
+      placeholder: 'Scene name',
       style: `
         display: flex;
         position: absolute;
         top: 10px;
         left: 10px;
-        width: 180px;
+        width: 140px;
       `,
       onChange: (event) => {
         this._editorScene.setName(event.target.value)
       },
     }),
     new Input({
+      name: 'saveScene',
+      type: 'button',
+      value: 'Save',
+      style: `
+        display: flex;
+        position: absolute;
+        top: 10px;
+        left: 166px;
+        width: 50px;
+      `,
+    }),
+    new Input({
+      name: 'loadScene',
+      type: 'button',
+      value: 'Load',
+      style: `
+        display: flex;
+        position: absolute;
+        top: 10px;
+        left: 226px;
+        width: 50px;
+      `,
+    }),
+    new Input({
       name: 'gridWidth',
       type: 'number',
-      placeholder: 'Grid width...',
+      placeholder: 'Width (max. 30)',
       style: `
         display: flex;
         position: absolute;
         top: 40px;
         left: 10px;
-        width: 80px;
+        width: 120px;
       `,
       defaultValue: `${this._editorGrid.getWidth()}`,
       onChange: (event) => {
-        this._editorGrid.setWidth(Number(event.target.value))
+        this._editorGrid.setWidth(this.clampGridInput(Number(event.target.value)))
       },
+      max: GRID_MAX_SIZE,
+      min: GRID_MIN_SIZE,
     }),
     new Input({
       name: 'gridDepth',
       type: 'number',
-      placeholder: 'Grid depth...',
+      placeholder: 'Depth (max. 30)',
       style: `
         display: flex;
         position: absolute;
         top: 40px;
-        left: 110px;
-        width: 80px;
+        left: 150px;
+        width: 120px;
       `,
       defaultValue: `${this._editorGrid.getDepth()}`,
       onChange: (event) => {
-        this._editorGrid.setDepth(Number(event.target.value))
+        this._editorGrid.setDepth(this.clampGridInput(Number(event.target.value)))
       },
+      max: GRID_MAX_SIZE,
+      min: GRID_MIN_SIZE,
     }),
     ]
   }
--- src/ui/input.ts
+++ src/ui/input.ts
@@ -5,9 +5,16 @@ interface Props {
   name?: string
   type?: string
   placeholder?: string
+  value?: string
   defaultValue?: string
   style?: string
   onChange?: (event: ChangeEvent) => void,
+  onClick?: (event: ChangeEvent) => void,
+  max?: number
+  min?: number
+  required?: boolean
+  pattern?: string
+  title?: string
 }
 
 export default class Input implements Component {
@@ -17,17 +24,44 @@ export default class Input implements Component {
   private _name: string
   private _type: string
   private _placeholder: string
+  private _value: string
   private _defaultValue: string
   private _style: string
   private _onChange: (event: ChangeEvent) => void
+  private _onClick: (event: ChangeEvent) => void
+  private _max: number
+  private _min: number
+  private _required: boolean
+  private _pattern: string
+  private _title: string
 
-  constructor({ name, type = 'text', placeholder, defaultValue, style, onChange }: Props) {
+  constructor({
+    name,
+    type = 'text',
+    placeholder,
+    defaultValue,
+    value,
+    onClick,
+    style,
+    onChange,
+    max,
+    min,
+    required,
+    pattern,
+    title,
+  }: Props) {
     this._name = name
     this._type = type
     this._placeholder = placeholder
+    this._value = value
     this._defaultValue = defaultValue
     this._style = style
+    this._max = max
+    this._min = min
     this._onChange = onChange
+    this._required = required
+    this._pattern = pattern
+    this._title = title
 
     this.start()
   }
@@ -38,18 +72,54 @@ export default class Input implements Component {
     this._instance.setAttribute('placeholder', this._placeholder)
     this._instance.setAttribute('type', this._type)
     this._instance.setAttribute('style', this._style)
+
+    if (this._value) {
+      this._instance.setAttribute('value', this._value)
+    }
     
     if (this._defaultValue) {
       this._instance.setAttribute('value', this._defaultValue)
     }
 
-    this._instance.addEventListener('input', this._onChange)
+    if (this._max) {
+      this._instance.setAttribute('max', `${this._max}`)
+    }
+    
+    if (this._min) {
+      this._instance.setAttribute('min', `${this._min}`)
+    }
+    
+    if (this._required) {
+      this._instance.setAttribute('required', `${this._required}`)
+    }
+
+    if (this._pattern) {
+      this._instance.setAttribute('pattern', `${this._pattern}`)
+    }
+
+    if (this._title) {
+      this._instance.setAttribute('title', `${this._title}`)
+    }
+
+    if (this._onChange) {
+      this._instance.addEventListener('input', this._onChange)
+    }
+
+    if (this._onClick) {
+      this._instance.addEventListener('click', this._onClick)
+    }
 
     document.body.appendChild(this._instance)
   }
 
   destroy () {
-    this._instance.removeEventListener('input', this._onChange)
+    if (this._onChange) {
+      this._instance.removeEventListener('input', this._onChange)
+    }
+
+    if (this._onClick) {
+      this._instance.removeEventListener('click', this._onClick)
+    }
 
     document.removeChild(this._instance)
   }
